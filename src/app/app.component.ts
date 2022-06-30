9e68d0f1e5a4d41a67bfae4c8a210e6844dbe571
TheJLifeX <44620382+TheJLifeX@users.noreply.github.com>
2022-06-30 19:00:48 +0200

    Check ngx-navigation-trigger compatibility with Angular 12.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationTriggerService } from 'ngx-navigation-trigger';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular12-project';
+export class AppComponent implements OnInit {
+
+  currentPageNavigationTrigger: any;
+
+  constructor(private navigationTriggerService: NavigationTriggerService) { }
+
+  ngOnInit(): void {
+    this.navigationTriggerService.eventUrlChanged.subscribe((value) => {
+      this.currentPageNavigationTrigger = value;
+    });
+  }
 }
